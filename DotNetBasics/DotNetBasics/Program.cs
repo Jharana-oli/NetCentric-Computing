4b03f8423005220f57bbf57548b4a90ba7acce6a
Jharana-oli <olijharana2004@gmail.com>
2025-07-17 22:37:32 +0545

    Add example code for generic collections in C#

---
+++ DotNetBasics/DotNetBasics/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            generics obj = new generics();
+            obj.MUL<int>(5, 6);
+            obj.DIV<float>(10.5f, 2.5f);
+
+        }
+    }
+}
---
+++ DotNetBasics/DotNetBasics/generics.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetBasics
+{
+    public class generics
+    {
+        public void ADD<T>(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine(d1 + d2);
+        }
+        public void SUB<T>(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine(d1 - d2);
+        }
+        public void MUL<T>(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine(d1 * d2);
+        }
+        public void DIV<T>(T a, T b)
+        {
+            dynamic d1 = a;
+            dynamic d2 = b;
+            Console.WriteLine(d1 / d2);
+        }
+        
+    }
+}
